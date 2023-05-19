cf4be3abed99562857523eb9b40753ebeaed2f65
Mohamed Magdy <mohamed.magdy@canary.is>
2023-05-19 17:46:02 +0200

    Update Apple Store reports to latest version
    - Add more reports
    - Update reports version to the latest
    - Update the Python libraries to the latest

--- setup.py
+++ setup.py
@@ -2,27 +2,23 @@
 
 from setuptools import setup
 
-setup(name='tap-appstore',
-      version='0.2.1-airbyte',
-      description='Singer.io tap for extracting data from the App Store Connect API',
-      author='JustEdro',
-      url='https://github.com/JustEdro',
-      classifiers=['Programming Language :: Python :: 3 :: Only'],
-      py_modules=['tap-appstore'],
+setup(name="tap-appstore",
+      version="0.2.2+airbyte",
+      description="Singer.io tap for extracting data from the App Store Connect API",
+      author="JustEdro",
+      url="https://github.com/JustEdro",
+      classifiers=["Programming Language :: Python :: 3 :: Only"],
+      py_modules=["tap-appstore"],
       install_requires=[
-          'singer-python==5.2.3',
-          'appstoreconnect==0.9.0',
-          'pytz==2018.4'
+          "singer-python==5.13.0",
+          "appstoreconnect==0.10.0",
+          "pytz==2023.3"
       ],
-      entry_points='''
+      entry_points="""
           [console_scripts]
           tap-appstore=tap_appstore:main
-      ''',
-      packages=['tap_appstore'],
-      package_data={
-          'tap_appstore/schemas': [
-              'summary_sales_report.json'
-          ],
-      },
+      """,
+      packages=["tap_appstore"],
+      package_data={},
       include_package_data=True,
-      )
+)
--- tap_appstore/__init__.py
+++ tap_appstore/__init__.py
@@ -1,57 +1,82 @@
 #!/usr/bin/env python3
 
+import json
+import os
 from datetime import datetime
 from datetime import timedelta
-import os
-import json
 from typing import Dict, Union, List
 
 import singer
-from singer import utils, metadata, Transformer
-
 from appstoreconnect import Api
 from appstoreconnect.api import APIError
-import pytz
+from singer import utils, metadata, Transformer
 
 REQUIRED_CONFIG_KEYS = [
-    'key_id',
-    'key_file',
-    'issuer_id',
-    'vendor',
-    'start_date'
+    "key_id",
+    "key_file",
+    "issuer_id",
+    "vendor",
+    "start_date"
 ]
 
 STATE = {}
 
 LOGGER = singer.get_logger()
 
-BOOKMARK_DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
-TIME_EXTRACTED_FORMAT = '%Y-%m-%dT%H:%M:%S%z'
+BOOKMARK_DATE_FORMAT = "%Y-%m-%d"
+
+SALES_REPORT = "sales_report"
+SUBSCRIPTION_REPORT = "subscription_report"
+SUBSCRIPTION_EVENT_REPORT = "subscription_event_report"
+SUBSCRIBER_REPORT = "subscriber_report"
+NEWSSTAND_REPORT = "newsstand_report"
+PRE_ORDER_REPORT = "pre_order_report"
+SUBSCRIPTION_OFFER_CODE_REDEMPTION_REPORT = "subscription_offer_code_redemption_report"
+
+REPORT_IDENTIFIER = "_report_date"
 
 API_REQUEST_FIELDS = {
-    'subscription_event_report': {
-        'reportType': 'SUBSCRIPTION_EVENT',
-        'frequency': 'DAILY',
-        'reportSubType': 'SUMMARY',
-        'version': '1_2'
+    SALES_REPORT: {
+        "reportType": "SALES",
+        "frequency": "DAILY",
+        "reportSubType": "SUMMARY",
+        "version": "1_0"
     },
-    'subscriber_report': {
-        'reportType': 'SUBSCRIBER',
-        'frequency': 'DAILY',
-        'reportSubType': 'DETAILED',
-        'version': '1_2'
+    SUBSCRIPTION_REPORT: {
+        "reportType": "SUBSCRIPTION",
+        "frequency": "DAILY",
+        "reportSubType": "SUMMARY",
+        "version": "1_3"
     },
-    'subscription_report': {
-        'reportType': 'SUBSCRIPTION',
-        'frequency': 'DAILY',
-        'reportSubType': 'SUMMARY',
-        'version': '1_2'
+    SUBSCRIPTION_EVENT_REPORT: {
+        "reportType": "SUBSCRIPTION_EVENT",
+        "frequency": "DAILY",
+        "reportSubType": "SUMMARY",
+        "version": "1_3"
     },
-    'sales_report': {
-        'reportType': 'SALES',
-        'frequency': 'DAILY',
-        'reportSubType': 'SUMMARY',
-        'version': '1_0'
+    SUBSCRIBER_REPORT: {
+        "reportType": "SUBSCRIBER",
+        "frequency": "DAILY",
+        "reportSubType": "DETAILED",
+        "version": "1_3"
+    },
+    NEWSSTAND_REPORT: {
+        "reportType": "NEWSSTAND",
+        "frequency": "DAILY",
+        "reportSubType": "DETAILED",
+        "version": "1_0"
+    },
+    PRE_ORDER_REPORT: {
+        "reportType": "PRE_ORDER",
+        "frequency": "DAILY",
+        "reportSubType": "SUMMARY",
+        "version": "1_0"
+    },
+    SUBSCRIPTION_OFFER_CODE_REDEMPTION_REPORT: {
+        "reportType": "SUBSCRIPTION_OFFER_CODE_REDEMPTION",
+        "frequency": "DAILY",
+        "reportSubType": "SUMMARY",
+        "version": "1_0"
     }
 }
 
@@ -68,7 +93,7 @@ class Context:
     @classmethod
     def get_catalog_entry(cls, stream_name):
         if not cls.stream_map:
-            cls.stream_map = {s["tap_stream_id"]: s for s in cls.catalog['streams']}
+            cls.stream_map = {s["tap_stream_id"]: s for s in cls.catalog["streams"]}
         return cls.stream_map.get(stream_name)
 
     @classmethod
@@ -80,19 +105,19 @@ class Context:
     def is_selected(cls, stream_name):
         stream = cls.get_catalog_entry(stream_name)
         if stream is not None:
-            stream_metadata = metadata.to_map(stream['metadata'])
-            return metadata.get(stream_metadata, (), 'selected')
+            stream_metadata = metadata.to_map(stream["metadata"])
+            return metadata.get(stream_metadata, (), "selected")
         return False
 
     @classmethod
     def print_counts(cls):
-        LOGGER.info('------------------')
+        LOGGER.info("------------------")
         for stream_name, stream_count in Context.new_counts.items():
-            LOGGER.info('%s: %d new, %d updates',
+            LOGGER.info("%s: %d new, %d updates",
                         stream_name,
                         stream_count,
                         Context.updated_counts[stream_name])
-        LOGGER.info('------------------')
+        LOGGER.info("------------------")
 
 
 def get_abs_path(path):
@@ -103,48 +128,44 @@ def get_abs_path(path):
 def load_schemas():
     schemas = {}
 
-    for filename in os.listdir(get_abs_path('schemas')):
-        path = get_abs_path('schemas') + '/' + filename
-        file_raw = filename.replace('.json', '')
+    for filename in os.listdir(get_abs_path("schemas")):
+        path = get_abs_path("schemas") + "/" + filename
+        file_raw = filename.replace(".json", "")
         with open(path) as file:
             schemas[file_raw] = json.load(file)
 
     return schemas
 
 
-def discover(api: Api):
+def discover():
     raw_schemas = load_schemas()
     streams = []
     for schema_name, schema in raw_schemas.items():
-        report_date = datetime.strptime(get_bookmark(schema_name), "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d")
-        filters = get_api_request_fields(report_date, schema_name)
-
-        report = _attempt_download_report(api, filters)
-        if report:
-            # create and add catalog entry
-            catalog_entry = {
-                'stream': schema_name,
-                'tap_stream_id': schema_name,
-                'schema': schema,
-                'key_properties': []
-            }
-            streams.append(catalog_entry)
+        catalog_entry = {
+            "type": "SCHEMA",
+            "stream": schema_name,
+            "tap_stream_id": schema_name,
+            "schema": schema,
+            "key_properties": [REPORT_IDENTIFIER],
+            "bookmark_properties": ["start_date"]
+        }
+        streams.append(catalog_entry)
 
     if len(streams) == 0:
         LOGGER.warning("Could not find any reports types to download for the input configuration.")
 
-    return {'streams': streams}
+    return {"streams": streams}
 
 
 def tsv_to_list(tsv):
-    lines = tsv.split('\n')
-    header = [s.lower().replace(' ', '_').replace('-', '_') for s in lines[0].split('\t')]
+    lines = tsv.split("\n")
+    header = [s.lower().replace(" ", "_").replace("-", "_") for s in lines[0].split("\t")]
     data = []
     for line in lines[1:]:
         if len(line) == 0:
             continue
         line_obj = {}
-        line_cols = line.split('\t')
+        line_cols = line.split("\t")
         for i, column in enumerate(header):
             if i < len(line_cols):
                 line_obj[column] = line_cols[i].strip()
@@ -156,13 +177,13 @@ def tsv_to_list(tsv):
 def get_api_request_fields(report_date, stream_name) -> Dict[str, any]:
     """Get fields to be used in appstore API request """
     report_filters = {
-        'reportDate': report_date,
-        'vendorNumber': f"{Context.config['vendor']}"
+        "reportDate": report_date,
+        "vendorNumber": f"{Context.config['vendor']}"
     }
 
     api_fields = API_REQUEST_FIELDS.get(stream_name)
     if api_fields is None:
-        raise Exception(f'API request fields not set to stream "{stream_name}"')
+        raise Exception(f"API request fields not set to stream '{stream_name}'")
     else:
         report_filters.update(API_REQUEST_FIELDS[stream_name])
 
@@ -171,9 +192,9 @@ def get_api_request_fields(report_date, stream_name) -> Dict[str, any]:
 
 def sync(api: Api):
     # Write all schemas and init count to 0
-    for catalog_entry in Context.catalog['streams']:
+    for catalog_entry in Context.catalog["streams"]:
         stream_name = catalog_entry["tap_stream_id"]
-        singer.write_schema(stream_name, catalog_entry['schema'], catalog_entry['key_properties'])
+        singer.write_schema(stream_name, catalog_entry["schema"], catalog_entry["key_properties"])
 
         Context.new_counts[stream_name] = 0
         Context.updated_counts[stream_name] = 0
@@ -186,7 +207,7 @@ def _attempt_download_report(api: Api, report_filters: Dict[str, any]) -> Union[
     try:
         rep_tsv = api.download_sales_and_trends_reports(filters=report_filters)
     except APIError as e:
-        LOGGER.error(e)
+        LOGGER.error(f"Report with filter {report_filters} with error: {e}")
         return None
 
     # parse api response
@@ -198,7 +219,7 @@ def _attempt_download_report(api: Api, report_filters: Dict[str, any]) -> Union[
 
 def query_report(api: Api, catalog_entry):
     stream_name = catalog_entry["tap_stream_id"]
-    stream_schema = catalog_entry['schema']
+    stream_schema = catalog_entry["schema"]
 
     # get bookmark from when data will be pulled
     bookmark = datetime.strptime(get_bookmark(stream_name), "%Y-%m-%dT%H:%M:%SZ").astimezone()
@@ -208,7 +229,7 @@ def query_report(api: Api, catalog_entry):
     singer.write_bookmark(
         Context.state,
         stream_name,
-        'start_date',
+        "start_date",
         current_date_iterator.strftime(BOOKMARK_DATE_FORMAT)
     )
 
@@ -221,26 +242,19 @@ def query_report(api: Api, catalog_entry):
             report_optional = _attempt_download_report(api, report_filters)
 
             if report_optional:
-                # write records
                 for index, line in enumerate(report_optional, start=1):
                     data = line
-                    data['_line_id'] = index
-                    data['_time_extracted'] = extraction_time.strftime(TIME_EXTRACTED_FORMAT)
-                    data['_api_report_date'] = report_date
-                    rec = transformer.transform(data, stream_schema)
+                    data[REPORT_IDENTIFIER] = report_date
+                    record = transformer.transform(data, stream_schema)
 
-                    singer.write_record(
-                        stream_name,
-                        rec,
-                        time_extracted=extraction_time
-                    )
+                    singer.write_record(stream_name, record)
 
                     Context.new_counts[stream_name] += 1
 
                 singer.write_bookmark(
                     Context.state,
                     stream_name,
-                    'start_date',
+                    "start_date",
                     (current_date_iterator + delta).strftime(BOOKMARK_DATE_FORMAT)
                 )
 
@@ -252,9 +266,9 @@ def query_report(api: Api, catalog_entry):
 
 
 def get_bookmark(name):
-    bookmark = singer.get_bookmark(Context.state, name, 'start_date')
+    bookmark = singer.get_bookmark(Context.state, name, "start_date")
     if bookmark is None:
-        bookmark = Context.config['start_date']
+        bookmark = Context.config["start_date"]
     return bookmark
 
 
@@ -265,14 +279,14 @@ def main():
 
     Context.config = args.config
     api = Api(
-        Context.config['key_id'],
-        Context.config['key_file'],
-        Context.config['issuer_id']
+        Context.config["key_id"],
+        Context.config["key_file"],
+        Context.config["issuer_id"]
     )
 
     # If discover flag was passed, run discovery mode and dump output to stdout
     if args.discover:
-        catalog = discover(api)
+        catalog = discover()
         Context.config = args.config
         print(json.dumps(catalog, indent=2))
 
@@ -281,11 +295,11 @@ def main():
         if args.catalog:
             Context.catalog = args.catalog.to_dict()
         else:
-            Context.catalog = discover(api)
+            Context.catalog = discover()
 
         Context.state = args.state
         sync(api)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
